88a55cdce11febc38bdad6c7632c2efde3e15969
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-04-16 18:27:34 +0200

    edited party list and potluck item at random

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items")
 
 addGuestButton.addEventListener("click", function (){
   const guest = guestInput.value;
@@ -22,15 +24,17 @@ addGuestButton.addEventListener("click", function (){
   }
 });
 
-const clearInput = function() {
-  guestInput.value = "";
-};
-
 const addToList = function(guest) {
   const listItem = document.createElement("li");
   listItem.innerText = guest;
   guestList.append(listItem);
 }
+
+const clearInput = function() {
+  guestInput.value = "";
+};
+
+
 
 const updateGuestCount = function() {
   const guests = document.querySelectorAll(".guest-list li");
@@ -42,3 +46,37 @@ const updateGuestCount = function() {
     guestFull.classList.remove("hide");
   }
 };
+
+const assignItems = function() {
+  const potluckItems = [
+    "hummus",
+    "cookies",
+    "salad",
+    "hotdogs",
+    "jello",
+    "pasta",
+    "hamburgers",
+    "drinks",
+    "plates",
+    "cutlery",
+    "juice",
+    "pizza"
+   ];
+
+   const allGuests = document.querySelectorAll(".guest-list li");
+   for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}`
+    assignedItems.append(listItem);
+
+    potluckItems.splice(randomPotluckIndex, 1);
+  };
+};
+
+assignButton.addEventListener("click", function( ){
+  assignItems();
+  assigned.disabled = true;
+});
