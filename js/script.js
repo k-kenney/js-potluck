8c845036a4f8180d75dbb8e45877f5ab6244a943
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-04-17 07:15:19 +0200

    changed guestList to guests.length === 8

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ const clearInput = function() {
 const updateGuestCount = function() {
   const guests = document.querySelectorAll(".guest-list li");
   guestCount.innerText = guests.length;
-  if (guestCount === 8) {
+  if (guests.length === 8) {
     addGuestButton.classList.add("hide");
     guestInput.classList.add("hide");
     guestInputLabel.classList.add("hide");
